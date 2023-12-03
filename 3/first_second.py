09b39eff7c3feabc2129cfa09ddbbb79805e9de7
Edvin Sinkevič <sinkevicedwin@gmail.com>
2023-12-03 08:49:10 +0200

    Clean up a bit

--- 3/first_second.py
+++ 3/first_second.py
@@ -1,6 +1,26 @@
-visited = set()
+def main():
+    with open("data.txt", "r") as f:
+        matrix = [*map(list, f.read().splitlines())]
 
-def find_adj_numbers(matrix, _i, _j):
+    current_sum1 = 0
+    current_sum2 = 0
+    visited = set()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            v = get_at(matrix, i, j)
+            if not v.isnumeric() and v != ".":
+                nums = find_adj_numbers(matrix, i, j, visited)
+                if len(nums) == 2 and v == "*":
+                    ratio = nums[0] * nums[1]
+                    current_sum2 += ratio
+                current_sum1 += sum(nums)
+
+    print(current_sum1)
+    print(current_sum2)
+
+
+def find_adj_numbers(matrix, _i, _j, visited):
     nums = []
     for (i, j) in adj_indices(_i, _j):
         v = get_at(matrix, i, j)
@@ -10,7 +30,8 @@ def find_adj_numbers(matrix, _i, _j):
                 num = read_number(matrix, i, start_j)
                 nums.append(num)
                 visited.add((i, start_j))
-    return nums 
+    return nums
+
 
 def go_to_start(matrix, i, j):
     curr = '0'
@@ -18,6 +39,7 @@ def go_to_start(matrix, i, j):
         j -= 1
         curr = get_at(matrix, i, j)
     return j + 1
+
 
 def read_number(matrix, i, j):
     curr = get_at(matrix, i, j)
@@ -28,37 +50,22 @@ def read_number(matrix, i, j):
         curr = get_at(matrix, i, j)
     return int(accum)
 
+
 def get_at(matrix, i, j):
     try:
         return matrix[i][j]
     except IndexError:
         return None
 
+
 def adj_indices(i, j):
     return [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1),
             (i + 1, j + 1), (i - 1, j + 1), (i + 1, j - 1),
             (i - 1, j - 1)]
 
+
 def isnumber(x):
     return x is not None and x.isnumeric()
 
 
-with open("data.txt", "r") as f:
-    matrix = [*map(list, f.read().splitlines())]
-
-current_sum1 = 0
-current_sum2 = 0
-
-for i in range(len(matrix)):
-    for j in range(len(matrix[0])):
-        v = get_at(matrix, i, j)
-        if not v.isnumeric() and v != ".":
-            nums = find_adj_numbers(matrix, i, j)
-            if len(nums) == 2 and v == "*":
-                ratio = nums[0] * nums[1]
-                current_sum2 += ratio
-            current_sum1 += sum(nums)
-
-print(current_sum1)
-print(current_sum2)
-
+main()
